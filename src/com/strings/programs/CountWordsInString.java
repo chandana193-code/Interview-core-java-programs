b28b56a8559acfa22d667a416f24d19e3d4c67e6
Nalla Chandana <nallachandana193@gmail.com>
2025-11-18 23:06:24 +0530

    Improve word count logic in CountWordsInString
    Refactor word counting logic to handle leading spaces.

--- src/com/strings/programs/CountWordsInString.java
+++ src/com/strings/programs/CountWordsInString.java
@@ -9,8 +9,10 @@ public class CountWordsInString {
 		String str =" java programming language ";
 		int count = 0;
 		for(int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == ' ' && str.charAt(i+1) != ' ') {
-				count++;
+			if ((i == 0 && str.charAt(i) != ' ') ||
+                (str.charAt(i) != ' ' && str.charAt(i-1) == ' ')) {
+                count++;
+            }
 			}
 		}
 		System.out.println(count);
